d182e412d7d5094a866861f50a013bcf146e2a29
Haris <hariswaseem143@gmail.com>
2022-12-22 18:27:18 +0500

    tree

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Haris Waseem</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is React App
         </p>
         <a
           className="App-link"
